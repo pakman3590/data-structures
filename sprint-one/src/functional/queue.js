0c8b973064a46cfb11601526d7f300f5a7e15728
Matthew Pak <mattpak@gmail.com>
2022-02-24 15:07:07 -0800

    Partially implemented functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,37 @@
+/*
+HIGH LEVEL:
+stores values within an object in order based on numeric keys
+adds new values to object at end (highest key) and removes lowest key
+*/
+
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+
+  // declare first index property
+  someInstance.firstKey = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add value with next numeric key to storage
+    this.storage[this.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    // remove object at first key from storage
+    delete this.storage[this.firstKey];
+    // update new first key
+    this.firstKey ++;
   };
 
   someInstance.size = function() {
+    // return number of keys in storage object
+    return Object.keys(this.storage).length;
   };
+
 
   return someInstance;
 };

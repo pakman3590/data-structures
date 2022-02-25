71e9cd6864540c945bb6288681c567d3b427e0be
Matthew Pak <mattpak@gmail.com>
2022-02-25 11:07:49 -0800

    Passed tests for pseudoclassical-stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // add storage object to this
+  this.storage = {};
+  // add current property to this
+  this.current = 0;
 };
 
-
+// add push method to prototype
+Stack.prototype.push = function(value) {
+  // add value to storage object at current key
+  this.storage[this.current] = value;
+  // increment current key
+  this.current ++;
+};
+// add pop mehtod to prototype
+Stack.prototype.pop = function() {
+  // store value at current key minus one
+  let deleted = this.storage[this.current - 1];
+  // delete value
+  delete this.storage[this.current - 1];
+  // decrement current key
+  this.current --;
+  // return value
+  return deleted;
+};
+// add size method to prototype
+Stack.prototype.size = function() {
+  // return length of storage object key array
+  return Object.keys(this.storage).length;
+};

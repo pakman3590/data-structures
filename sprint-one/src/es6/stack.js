ea5f49b57f7c818ba89d96cfbc549fd8634f6713
Matthew Pak <mattpak@gmail.com>
2022-02-25 11:19:27 -0800

    Passed es6-stack tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,32 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    // add storage object to this
+    this.storage = {};
+    // add current property to this
+    this.current = 0;
   }
-
+  // add push method
+  push(value) {
+    // add value to storage object at current key
+    this.storage[this.current] = value;
+    // increment current key
+    this.current ++;
+  }
+  // add pop method
+  pop() {
+    // store value at current key minus one
+    let deleted = this.storage[this.current - 1];
+    // delete value
+    delete this.storage[this.current - 1];
+    // decrement current key
+    this.current --;
+    // return value
+    return deleted;
+  }
+  // add size method
+  size() {
+    // return length of storage object key array
+    return Object.keys(this.storage).length;
+  }
 }
